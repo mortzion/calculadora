a2b435eeb4d511aa8e6a6f9661509b73baaf8e5b
YiazmaT <eymarlima@gmail.com>
2015-12-18 18:24:17 -0200

    varias parada nego.

--- Calculadora/telaPrincipal.cpp
+++ Calculadora/telaPrincipal.cpp
@@ -6,7 +6,8 @@ using namespace System::Windows::Forms;
 using namespace std;
 
 int main(void) {
-	
+	//mort viadao chupa rola e os karai ;) <3 me come pf, vc é mt gato, me come serio, <3, chupo rola, faço anal giratorio
+	//se achar pouco trago uma amiga, 
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 
